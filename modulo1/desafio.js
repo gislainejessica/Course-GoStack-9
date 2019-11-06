e039ec80af6bef98d0b88957dd5f714471b579f9
GislaineJessica <GislainehJessica@gmail.com>
2019-11-06 11:50:32 -0300

    desafio 1

--- modulo1/desafio.js
+++ modulo1/desafio.js
@@ -3,57 +3,69 @@ const server = express()
 
 server.use(express.json())
 
-const users = ['Gislaine', 'Jéssica', 'Nina', 'Pietro']
+const projects = []
 
-// Middlewares globais internas
-server.use((req, res, next) => {
-  console.time('requisicao')
-  console.log(`método: ${req.method}; url:${req.url}`)
-  next()
-  console.timeEnd('requisicao')
-})
-// Middleware em forma de função
-function checkUserExists(req, res, next) {
-  if (!req.body.name) {
-    return res.status(400).json({ error: 'Nome de usuario obrigatorio' })
+/** Middlewares */
+function checkProjectExists(req, res, next) {
+  const { id } = req.params
+  const project = projects.find(p => p.id === id)
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' })
   }
   return next()
 }
-
-function checkUserInArray(req, res, next) {
-  if (!users[req.params.index]) {
-    return res.status(400).json({ error: 'Usuario não existe' })
-  }
+function logRequests(req, res, next) {
+  console.count('Número de requisições')
   return next()
 }
-// Rotas que iam ser expostas
-server.get('/users', (req, res) => {
-  return res.json(users)
-})
 
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params
-  return res.json(users[index])
-})
+server.use(logRequests)
 
-server.post('/users', checkUserExists, (req, res) => {
-  const { name } = req.body
-  users.push(name)
-  return res.json(users)
+/** ROTAS */
+server.get('/projects', (req, res) => {
+  return res.json(projects)
 })
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body
 
-server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
-  const { index } = req.params
-  const { name } = req.body
-  users[index] = name
-  return res.json(users)
+  const project = {
+    id,
+    title,
+    tasks: [],
+  }
+
+  projects.push(project)
+
+  return res.json(project)
 })
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
 
-server.delete('/users/:index', checkUserInArray, (req, res) => {
-  const { index } = req.params.index
-  users.splice(index, 1)
-  res.send()
+  const project = projects.find(p => p.id === id)
+
+  project.title = title
+
+  return res.json(project)
 })
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params
 
-// Ouvindo a PORTA
-server.listen(3000)
+  const projectIndex = projects.findIndex(p => p.id === id)
+
+  projects.splice(projectIndex, 1)
+
+  return res.send()
+})
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  const project = projects.find(p => p.id === id)
+
+  project.tasks.push(title)
+
+  return res.json(project)
+})
+server.listen(4444)
